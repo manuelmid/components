8a63e4a4ba363084f07c933847589b429a062b59
Manuel <manuelmid99@gmail.com>
2023-05-11 13:54:54 +0200

    slider_5 created

---
+++ slider_5/scriptES5.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var wrapper = document.querySelector('.wrapper');
+var carousel = document.querySelector('.carousel');
+var firstCardWidth = carousel.querySelector('.card').offsetWidth;
+var arrowBtns = document.querySelectorAll('.wrapper i');
+var carouselChildrens = [].concat(Array.from(carousel.children));
+
+var isDragging = false,
+    isAutoPlay = false,
+    startX,
+    startScrollLeft,
+    timeoutId;
+
+var cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(function (card) {
+    carousel.insertAdjacentHTML('afterbegin', card.outerHTML);
+});
+
+carouselChildrens.slice(0, cardPerView).forEach(function (card) {
+    carousel.insertAdjacentHTML('beforeend', card.outerHTML);
+});
+
+carousel.classList.add('no-transition');
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove('no-transition');
+
+arrowBtns.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        carousel.scrollLeft += btn.id == 'left' ? -firstCardWidth : firstCardWidth;
+    });
+});
+
+var dragStart = function dragStart(e) {
+    isDragging = true;
+    carousel.classList.add('dragging');
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+};
+
+var dragging = function dragging(e) {
+    if (!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+var dragStop = function dragStop() {
+    isDragging = false;
+    carousel.classList.remove('dragging');
+};
+
+var infiniteScroll = function infiniteScroll() {
+    if (carousel.scrollLeft === 0) {
+        carousel.classList.add('no-transition');
+        carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+        carousel.classList.remove('no-transition');
+    } else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add('no-transition');
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove('no-transition');
+    }
+
+    clearTimeout(timeoutId);
+    if (!wrapper.matches(':hover')) autoPlay();
+};
+
+var autoPlay = function autoPlay() {
+    if (window.innerWidth < 800 || !isAutoPlay) return;
+    timeoutId = setTimeout(function () {
+        return carousel.scrollLeft += firstCardWidth;
+    }, 500);
+};
+autoPlay();
+
+carousel.addEventListener('mousedown', dragStart);
+carousel.addEventListener('mousemove', dragging);
+document.addEventListener('mouseup', dragStop);
+carousel.addEventListener('scroll', infiniteScroll);
+wrapper.addEventListener('mouseenter', function () {
+    return clearTimeout(timeoutId);
+});
