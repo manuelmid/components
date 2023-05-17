28f70c70f5a81f6a210a0eb86d2f4962b2ee74cf
Manuel <manuelmid99@gmail.com>
2023-05-17 14:46:35 +0200

    slider_5 working

--- slider_5/scriptES5.js
+++ slider_5/scriptES5.js
@@ -47,12 +47,14 @@ var dragging = function dragging(e) {
 var dragStop = function dragStop() {
     isDragging = false;
     carousel.classList.remove('dragging');
+
+    //To check up
 };
 
 var infiniteScroll = function infiniteScroll() {
     if (carousel.scrollLeft === 0) {
         carousel.classList.add('no-transition');
-        carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+        carousel.scrollLeft = carousel.scrollWidth - 1 * carousel.offsetWidth;
         carousel.classList.remove('no-transition');
     } else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
         carousel.classList.add('no-transition');
